a4e6fc080fb7da6e15b29c1f5b7f7745dfc8f729
Jhoseph Zamora <jhosephzc@gmail.com>
2026-03-06 12:53:37 -0500

    fix: fix route order to prevent /new matching :id

--- productos-front/src/App.tsx
+++ productos-front/src/App.tsx
@@ -10,10 +10,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<AuthGuard><ProductsList /></AuthGuard>} />
         <Route path="/products/new" element={<AuthGuard><ProductForm /></AuthGuard>} />
         <Route path="/products/:id/edit" element={<AuthGuard><ProductForm /></AuthGuard>} />
         <Route path="/products/:id" element={<AuthGuard><ProductDetail /></AuthGuard>} />
+        <Route path="/products" element={<AuthGuard><ProductsList /></AuthGuard>} />
         <Route path="*" element={<Navigate to="/products" replace />} />
       </Routes>
     </BrowserRouter>
